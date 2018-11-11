73b170911c744437198867a4f3197543093ceca4
Chrissy Zwainz <zchrissychrissy@gmail.com>
2018-11-11 13:10:44 -0700

    Lots Of Updates

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -1,15 +1,34 @@
 import ItunesService from "./itunes-service.js";
+import Song from "../../models/Song.js";
 
 //PRIVATE
 
 const itunesService = new ItunesService()
 
-function drawSongs(results) {
-  console.log(results)
+function drawSongs(songs) {
+  console.log(songs)
   //YOUR CODING STARTS HERE
-
-
-
+  let template = ''
+  for (let i = 0; i < songs.length; i++) {
+    let song = songs[i];
+    template += `
+      <div class="card text-center card-width">
+        <img src="${song.albumArt}" class="card-img-top"/>
+        <h3 class="card-title" style="text-shadow: 0px 0px 3px whitesmoke;"><strong>${song.title}</strong></h3>
+        <h4 style="text-shadow: 0px 0px 3px grey;">${song.collection}</h4>
+        <audio controls class="audio-width"><source src="${song.preview}" type="audio/ogg"></audio>
+      </div>
+      `
+  }
+  document.addEventListener('play', function (event) {
+    let audios = document.getElementsByTagName('audio');
+    for (let i = 0; i < audios.length; i++) {
+      if (audios[i] != event.target) {
+        audios[i].pause();
+      }
+    }
+  }, true);
+  document.getElementById('songs').innerHTML = template
 }
 
 
@@ -20,7 +39,7 @@ class ItunesController {
     e.preventDefault();
     var artist = e.target.artist.value;
     //changes the button to loading while songs load
-    $('#get-music-button').text('LOADING....');
+    $('#get-music-button').text('LOADING...');
     itunesService.getMusicByArtist(artist).then(results => {
       drawSongs(results)
       //changes button back to GET MUSIC once songs are loaded
